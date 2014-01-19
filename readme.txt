69b11a3b0dad6fd97ec8416acccf45e25cae7d10
Robert Hafner <tedivm@tedivm.com>
2014-01-18 23:59:34 -0800

    Added initial plugin files.

---
+++ readme.txt
@@ -0,0 +1,14 @@
+=== SyntaxHighlighter Evolved: Yaml Brush ===
+Contributors: tedivm
+Tags: syntaxhighlighting, yaml
+Requires at least: 2.7
+Tested up to: 3.8
+License: MIT
+
+Adds support for the Yaml language to the SyntaxHighlighter Evolved plugin.
+
+== Installation ==
+
+1. Make sure the Syntaxhighlighter Evolved plugin is installed.
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
---
+++ syntaxhighlighter_yaml.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: SyntaxHighlighter Evolved: Yaml Brush
+Description: Adds support for the Yaml language to the SyntaxHighlighter Evolved plugin.
+Author: Robert Hafner
+Version: 1.0.0
+Author URI: http://blog.tedivm.com/
+*/
+ 
+// SyntaxHighlighter Evolved doesn't do anything until early in the "init" hook, so best to wait until after that
+add_action( 'init', 'syntaxhighlighter_yaml_regscript' );
+ 
+// Tell SyntaxHighlighter Evolved about this new language/brush
+add_filter( 'syntaxhighlighter_brushes', 'syntaxhighlighter_yaml_addlang' );
+ 
+// Register the brush file with WordPress
+function syntaxhighlighter_yaml_regscript() {
+    wp_register_script( 'syntaxhighlighter-brush-yaml', plugins_url( 'shBrushYaml.js', __FILE__ ), array('syntaxhighlighter-core'), '1.2.3' );
+}
+ 
+// Filter SyntaxHighlighter Evolved's language array
+function syntaxhighlighter_yaml_addlang( $brushes ) {
+    $brushes['yaml'] = 'yaml';
+    $brushes['yml'] = 'yaml';
+ 
+    return $brushes;
+}
+ 
+?>
